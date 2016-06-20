85189aa29c65665818cdd918bc1c526029d496cb
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-20 18:40:36 -0300

    Add social networks

---
+++ src/store/social.js
@@ -0,0 +1,9 @@
+export default {
+  state: {
+    networks: [
+      {name: 'facebook', link: 'http://facebook.com/mauri870'},
+      {name: 'github', link: 'http://github.com/mauri870'},
+      {name: 'linkedin', link: '#'}
+    ]
+  }
+}
