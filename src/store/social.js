bc12ce65103da4212efd49595051ea10bbd6d69c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-20 19:16:44 -0300

    Change linkedin url

--- src/store/social.js
+++ src/store/social.js
@@ -3,7 +3,7 @@ export default {
     networks: [
       {name: 'facebook', link: 'http://facebook.com/mauri870'},
       {name: 'github', link: 'http://github.com/mauri870'},
-      {name: 'linkedin', link: '#'}
+      {name: 'linkedin', link: 'https://br.linkedin.com/in/mauri-de-souza-nunes-0b223090'}
     ]
   }
 }
