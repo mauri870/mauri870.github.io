8603c9509687276b38803998b49577b807172b1d
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-21 10:18:30 -0300

    Add steam profile

--- src/store/social.js
+++ src/store/social.js
@@ -3,7 +3,8 @@ export default {
     networks: [
       {name: 'facebook', link: 'http://facebook.com/mauri870'},
       {name: 'github', link: 'http://github.com/mauri870'},
-      {name: 'linkedin', link: 'https://br.linkedin.com/in/mauri-de-souza-nunes-0b223090'}
+      {name: 'linkedin', link: 'https://br.linkedin.com/in/mauri-de-souza-nunes-0b223090'},
+      {name: 'steam', link: 'http://steamcommunity.com/id/mauri870'}
     ]
   }
 }
