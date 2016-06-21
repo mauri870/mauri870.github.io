8b08d32513726986dca5b2945cefb00346045476
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-21 12:42:43 -0300

    Update info phrase

--- src/store/info.js
+++ src/store/info.js
@@ -1,6 +1,6 @@
 export default {
   state: {
-    phrase: 'Web Designer, Developer & Gamer',
+    phrase: 'Web Designer, Developer, Gamer, History, science and astronomy lover',
     email: 'mauri870@gmail.com',
     phone: '+55 54 8118-9792'
   }
