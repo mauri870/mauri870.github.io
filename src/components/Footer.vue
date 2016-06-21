301318149edab760e93e19f012368d2b83892bde
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-21 12:54:09 -0300

    Add section for source code to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,6 +13,11 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <p>Find the <i class="fa fa-code"></i> of this project on <a href="http://github.com/mauri870/mauri870.github.io" target="_blank"><i class="fa fa-github fa-2x"></i></a></p>
+      </div>
+    </div>
   </div>
 </template>
 
