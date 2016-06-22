b0f85c8e9b40b24844f586829108e68a7ad0f036
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-21 22:41:16 -0300

    Fix bug when footer breaks the template

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,8 +12,6 @@
           <p><a href="mailto:{{ info.email }}">{{ info.email }}</a></p>
         </div>
       </div>
-    </div>
-    <div class="row">
       <div class="col-sm-12 text-center">
         <p>Find the <i class="fa fa-code"></i> of this project on <a href="http://github.com/mauri870/mauri870.github.io" target="_blank"><i class="fa fa-github fa-2x"></i></a></p>
       </div>
