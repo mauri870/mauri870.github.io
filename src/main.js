dda51a3ea4c566a34de71cb85a974ee30c89b0d5
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-22 09:34:42 -0300

    Remove unused statements

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 // Import default styles
-import '../bower_components/fakeLoader/fakeLoader.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
 import 'font-awesome/css/font-awesome.css'
 
-import $ from 'jquery'
 import Vue from 'vue'
 import Site from './Site'
 
-import '../bower_components/fakeLoader/fakeLoader.min'
-
 /* eslint-disable no-new */
-var app = new Vue({
-  init () {
-    // Init Fakeloader
-    $('#fakeLoader').fakeLoader({spinner: 'spinner1', bgColor: '#1ABC9C'})
-     // Init tooltips
-    $(document).ready(() => { $('[data-toggle="tooltip"]').tooltip() })
-  },
-  components: { Site }
-})
+var app = new Vue({components: { Site }})
 
 app.$mount('body')
