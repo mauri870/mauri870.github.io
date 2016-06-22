02816da30cf8b4e09a9417bbd660a9a4507c1970
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-22 09:34:28 -0300

    Add component for load fakeloader

---
+++ src/components/fakeloader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="fakeLoader"></div>
+</template>
+
+<script>
+import $ from 'jquery'
+import '../../bower_components/fakeLoader/fakeLoader.min'
+
+export default {
+  ready () {
+    // Init Fakeloader
+    $('#fakeLoader').fakeLoader({spinner: 'spinner1', bgColor: '#1ABC9C'})
+  }
+}
+</script>
+
+<style>
+  @import '../../bower_components/fakeLoader/fakeLoader.css';
+</style>
