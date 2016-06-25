43d30c6f06098d7296a9e0540466188ac575adea
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-25 20:59:50 -0300

    Fix malformed store object

--- src/store/modules/info.js
+++ src/store/modules/info.js
@@ -1,5 +1,5 @@
 export default {
-  store: {
+  state: {
     phrase: 'Web Designer, Developer, Gamer, History, science and astronomy lover',
     email: 'mauri870@gmail.com',
     phone: '+55 54 8118-9792'
