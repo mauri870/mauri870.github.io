9354ea3c70a59b8980788989bb2eb64bc79d3599
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-25 20:14:19 -0300

    Init vuex store and add some modules

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import info from './modules/info'
+import projects from './modules/projects'
+import social from './modules/social'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    info,
+    projects,
+    social
+  }
+})
