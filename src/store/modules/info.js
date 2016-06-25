52b0c4ae41c65ecea0956d6e238a853470056e21
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-25 20:14:53 -0300

    Update old stores for vuex

---
+++ src/store/modules/info.js
@@ -0,0 +1,8 @@
+export default {
+  store: {
+    phrase: 'Web Designer, Developer, Gamer, History, science and astronomy lover',
+    email: 'mauri870@gmail.com',
+    phone: '+55 54 8118-9792'
+  },
+  mutations: {}
+}
---
+++ src/store/modules/projects.js
@@ -0,0 +1,43 @@
+export default {
+  state: {
+    projects: [
+      {
+        name: 'Cloudradioo App',
+        link: 'http://github.com/devfake/cloudradioo-app',
+        description: 'Cloudradioo App is a desktop application builded with Electron, VueJs and Vuex that plays randomly all top 50 songs from the soundcloud charts.',
+        image: require('../../assets/img/portfolio/cloudradioo-app.png')
+      },
+      {
+        name: 'mauri870/api-skeleton-laravel',
+        link: 'https://github.com/mauri870/api-skeleton-laravel',
+        description: 'An API Skeleton for Laravel 5.1 with JWT',
+        image: require('../../assets/img/portfolio/laravel-package.png')
+      },
+      {
+        name: 'artesaos/laravel-docs',
+        link: 'https://github.com/artesaos/laravel-docs',
+        description: 'Translation of Laravel 5 docs to PT-BR',
+        image: require('../../assets/img/portfolio/artesaos-package.png')
+      },
+      {
+        name: 'artesaos/laravel-installer',
+        link: 'https://github.com/artesaos/laravel-installer',
+        description: 'Install laravel framework with any version you want',
+        image: require('../../assets/img/portfolio/artesaos-package.png')
+      },
+      {
+        name: 'artesaos/defender',
+        link: 'https://github.com/artesaos/defender',
+        description: 'Roles & Permissions for Laravel 5',
+        image: require('../../assets/img/portfolio/artesaos-package.png')
+      },
+      {
+        name: 'artesaos/laravel-linkedin',
+        link: 'https://github.com/artesaos/laravel-linkedin',
+        description: 'Linkedin API integration for Laravel and Lumen 5',
+        image: require('../../assets/img/portfolio/artesaos-package.png')
+      }
+    ]
+  },
+  mutations: {}
+}
