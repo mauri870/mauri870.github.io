2c6c32fbfe7b464c26eb02c9f48f578f4b742fbd
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-26 14:32:59 -0300

    Init GA and remove unused eslint comments

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 import Site from './Site'
 import store from './store'
+import {initAnalytics} from './utils/analytics'
 
-/* eslint-disable no-new */
-/* eslint-disable no-unused-vars */
+initAnalytics('UA-79904458-1')
+
 var app = new Vue({
   store,
   components: { Site }
