cd81e55e03bab56c6f9bb1062fffe65ab17f5b9c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-27 20:27:11 -0300

    Init vue router

--- src/main.js
+++ src/main.js
@@ -7,12 +7,10 @@ import Vue from 'vue'
 import Site from './Site'
 import store from './store'
 import {initAnalytics} from './utils/analytics'
+import router from './router'
 
 initAnalytics('UA-79904458-1')
 
-var app = new Vue({
-  store,
-  components: { Site }
-})
+var app = Vue.extend({store, components: {Site}})
 
-app.$mount('body')
+router.start(app, 'body')
