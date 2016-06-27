bd573e595937b6d174ab5c714f0a0a409d55a909
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-27 20:31:12 -0300

    Move site content to index page

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template lang="jade">
+  header-wrap
+
+  my-portfolio
+</template>
+
+<script>
+    import HeaderWrap from '../components/Header.vue'
+    import MyPortfolio from '../components/Portfolio.vue'
+    export default {
+      components: {
+        HeaderWrap,
+        MyPortfolio
+      }
+    }
+</script>
