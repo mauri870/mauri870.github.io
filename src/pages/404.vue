baa51b01f4d36f490f436cd282863ebd8749e58b
Mauri de Souza Nunes <mauri870@gmail.com>
2016-06-27 20:31:31 -0300

    Add simple 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,25 @@
+<template lang="jade">
+  #content
+    .container
+      .row
+        .col-lg-6.col-lg-offset-3
+          h3 Sorry but the page you are looking for could not be found
+  .span#control-navbar     
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+  @import "../assets/sass/mixins.scss";
+
+  #content {
+    @include content-default
+
+    h3 {
+      font-weight: 400;
+      color: #000000;
+    }
+  }
+</style>
