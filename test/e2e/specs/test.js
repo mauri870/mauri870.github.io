c934c6853db4b60ce65b0bde944b258f4804bf1f
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-02 21:34:02 -0300

    Improve e2e tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -1,12 +1,20 @@
 module.exports = {
-  'default e2e tests': function (browser) {
+  'mauri870.github.io e2e tests': function (browser) {
     browser
     .url('http://localhost:8080')
+      .pause(1000)
       .waitForElementVisible('#fakeLoader', 5000)
-      .waitForElementVisible('#headerwrap', 2000)
+      .waitForElementVisible('#headerwrap', 5000)
+      .assert.title('Mauri de Souza Nunes - Website')
       .assert.containsText('h3', 'My skills')
       .assert.elementPresent('.logo')
       .assert.elementCount('.gallery', 6)
+      .end()
+  },
+  'mauri870.github.io e2e test 404': function (browser) {
+    browser
+    .url('http://localhost:8080/!#/whatever')
+      .assert.containsText('h1', 'Sorry but the page you are looking for could not be found')
       .end()
   }
 }
