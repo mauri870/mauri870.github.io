95b316acf35daeda58ddf56769be78d1b664575d
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-04 08:28:47 -0300

    Add Hire me link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,6 +9,7 @@
           span.icon-bar
           span.icon-bar
         a.navbar-brand.logo(v-link="{ path: '/' }") M
+        a#hiring(href="#" @click.prevent="clickHireMe") Hire Me!
       .navbar-collapse.collapse
         ul.nav.navbar-nav.navbar-right
           li
@@ -20,6 +21,16 @@
 import $ from 'jquery'
 
 export default {
+  vuex: {
+    getters: {
+      email: ({info}) => info.email
+    }
+  },
+  methods: {
+    clickHireMe () {
+      window.location.href = `mailto:${this.email}`
+    }
+  },
   ready () {
     $(window).on('scroll', function () {
       let navbar = $('#navbar-menu')
@@ -48,6 +59,49 @@ export default {
 
     .logo {
       font-size: xx-large;
+    }
+
+    a#hiring {
+      position: relative;
+      top: 45px;
+      background: $default-color;
+      padding: 7px 15px;
+      color: white;
+      display: inline;
+      top: 15px;
+      text-transform: uppercase;
+      letter-spacing: 1.3px;
+      text-decoration: none;
+      font-size: 13px;
+      -webkit-border-radius: 3px;
+      -moz-border-radius: 3px;
+      -ms-border-radius: 3px;
+      -o-border-radius: 3px;
+      border-radius: 3px;
+      font-weight: 600;
+      margin-top: 21px;
+      -webkit-transition: all 0.2s ease-in-out;
+      -moz-transition: all 0.2s ease-in-out;
+      -ms-transition: all 0.2s ease-in-out;
+      -o-transition: all 0.2s ease-in-out;
+      transition: all 0.2s ease-in-out;
+
+      &:after {
+        position: absolute;
+        content: "";
+        left: -5px;
+        top: 10px;
+        width: 0;
+        height: 0;
+        border-top: 5px solid transparent;
+        border-bottom: 5px solid transparent;
+        border-right: 5px solid $default-color;
+        -webkit-transition: all 0.2s ease-in-out;
+        -moz-transition: all 0.2s ease-in-out;
+        -ms-transition: all 0.2s ease-in-out;
+        -o-transition: all 0.2s ease-in-out;
+        transition: all 0.2s ease-in-out;
+      }
     }
     
     .navbar-nav {
