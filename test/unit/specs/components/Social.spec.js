5d39ce75c7c2e4549feef8fc4ed44e8c3b582106
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-04 14:15:25 -0300

    Update social test

--- test/unit/specs/components/Social.spec.js
+++ test/unit/specs/components/Social.spec.js
@@ -12,12 +12,13 @@ describe('Social.vue', () => {
     expect(vm.$el.querySelectorAll('#social .row .col-lg-3').length).to.eql(4)
   })
 
-  it('should render correct networks', () => {
+  it('should render correct networks section', () => {
     const vm = new Vue({
       template: '<div><social></social></div>',
       components: { Social },
       store
     }).$mount()
+    expect(vm.$el.querySelector('#social .row .col-lg-12 h2').textContent).to.eql('Stay connected!')
     expect(vm.$el.querySelectorAll('#social .row .col-lg-3 a')[0].href).to.contain('facebook')
     expect(vm.$el.querySelectorAll('#social .row .col-lg-3 a')[1].href).to.contain('github')
     expect(vm.$el.querySelectorAll('#social .row .col-lg-3 a')[2].href).to.contain('linkedin')
