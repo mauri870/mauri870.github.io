f47ec84c7f1a6f791fd948cd1e1f88b374977c82
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-04 14:12:25 -0300

    Test the curricula links

--- test/unit/specs/components/Header.spec.js
+++ test/unit/specs/components/Header.spec.js
@@ -9,4 +9,16 @@ describe('Header.vue', () => {
     }).$mount()
     expect(vm.$el.querySelector('h3').textContent).to.eql('My skills')
   })
+
+  it('should render correct curriculum links', () => {
+    const vm = new Vue({
+      template: '<div><vc-header></vc-header></div>',
+      components: { VcHeader: Header }
+    }).$mount()
+    expect(vm.$el.querySelectorAll('.curriculum a')[0].textContent).to.eql('Curriculum EN')
+    expect(vm.$el.querySelectorAll('.curriculum a')[0].href).to.contains('static/docs/curricula/curriculum-en.pdf')
+
+    expect(vm.$el.querySelectorAll('.curriculum a')[2].textContent).to.eql('Curriculum PT-BR')
+    expect(vm.$el.querySelectorAll('.curriculum a')[2].href).to.contains('static/docs/curricula/curriculum-pt.pdf')
+  })
 })
