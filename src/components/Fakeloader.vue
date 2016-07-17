03d1ded3ee411b75c7141c49876dedbe253d2e5c
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-16 23:04:12 -0300

    Change deprecated ready to mounted Run fakeloader init on document ready

--- src/components/Fakeloader.vue
+++ src/components/Fakeloader.vue
@@ -7,9 +7,11 @@ import $ from 'jquery'
 import '../../bower_components/fakeLoader/fakeLoader.min'
 
 export default {
-  ready () {
-    // Init Fakeloader
-    $('#fakeLoader').fakeLoader({spinner: 'spinner1', bgColor: '#1ABC9C'})
+  mounted () {
+    $(document).ready(function () {
+      // Init Fakeloader
+      $('#fakeLoader').fakeLoader({spinner: 'spinner1', bgColor: '#1ABC9C'})
+    })
   }
 }
 </script>
