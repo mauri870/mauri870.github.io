6db28050cb8ab6bda239e38a755f62e831d8f571
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-17 14:57:38 -0300

    Vuex Getters will be moved inside modules

--- src/store/getters.js
+++
@@ -1,7 +0,0 @@
-export const networks = ({social}) => social.networks
-
-export const projects = ({projects}) => projects.projects
-
-export const infoPhrase = ({info}) => info.phrase
-
-export const infoEmail = ({info}) => info.email
