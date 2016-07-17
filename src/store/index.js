32f7cfbf1341ad6f1dd6f2d9fc4031d2171bc8fd
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-16 23:05:47 -0300

    Add getters to vuex instance

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,12 @@ import Vuex from 'vuex'
 import info from './modules/info'
 import projects from './modules/projects'
 import social from './modules/social'
+import * as getters from './getters'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  getters,
   modules: {
     info,
     projects,
