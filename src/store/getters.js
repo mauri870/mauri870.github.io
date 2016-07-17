6938dea01fd47229b9d4c86a2354e435e652d3b0
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-16 23:05:17 -0300

    Add getters for vuex

---
+++ src/store/getters.js
@@ -0,0 +1,7 @@
+export const networks = state => state.social.networks
+
+export const projects = state => state.projects.projects
+
+export const infoPhrase = state => state.info.phrase
+
+export const infoEmail = state => state.info.email
