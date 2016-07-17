6ce94224ca41e0423af1934c96a1bae5295f17bb
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-16 23:06:29 -0300

    Add icon property to each network

--- src/store/modules/social.js
+++ src/store/modules/social.js
@@ -1,10 +1,10 @@
 export default {
   state: {
     networks: [
-      {name: 'facebook', link: 'http://facebook.com/mauri870'},
-      {name: 'github', link: 'http://github.com/mauri870'},
-      {name: 'linkedin', link: 'https://br.linkedin.com/in/mauri-de-souza-nunes-0b223090'},
-      {name: 'steam', link: 'http://steamcommunity.com/id/mauri870'}
+      {name: 'facebook', link: 'http://facebook.com/mauri870', icon: 'fa fa-facebook'},
+      {name: 'github', link: 'http://github.com/mauri870', icon: 'fa fa-github'},
+      {name: 'linkedin', link: 'https://br.linkedin.com/in/mauri-de-souza-nunes-0b223090', icon: 'fa fa-linkedin'},
+      {name: 'steam', link: 'http://steamcommunity.com/id/mauri870', icon: 'fa fa-steam'}
     ]
   },
   mutations: {}
