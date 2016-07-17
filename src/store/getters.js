130a15e0d7eb65f898019a0695e44c5f0c477d09
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-17 13:55:35 -0300

    Refactor to es6 object destructuring

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,7 @@
-export const networks = state => state.social.networks
+export const networks = ({social}) => social.networks
 
-export const projects = state => state.projects.projects
+export const projects = ({projects}) => projects.projects
 
-export const infoPhrase = state => state.info.phrase
+export const infoPhrase = ({info}) => info.phrase
 
-export const infoEmail = state => state.info.email
+export const infoEmail = ({info}) => info.email
