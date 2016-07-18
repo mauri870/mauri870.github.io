b86cf610adcdfe1f7924016ce4d9e55a294ad975
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-18 14:42:47 -0300

    Update header unit tests

--- test/unit/specs/components/Header.spec.js
+++ test/unit/specs/components/Header.spec.js
@@ -15,10 +15,13 @@ describe('Header.vue', () => {
       template: '<div><vc-header></vc-header></div>',
       components: { VcHeader: Header }
     }).$mount()
-    expect(vm.$el.querySelectorAll('.curriculum a')[0].textContent).to.eql('Curriculum EN')
+    expect(vm.$el.querySelectorAll('.curriculum a')[0].textContent).to.contain('Curriculum EN')
     expect(vm.$el.querySelectorAll('.curriculum a')[0].href).to.contains('static/docs/curricula/curriculum-en.pdf')
 
-    expect(vm.$el.querySelectorAll('.curriculum a')[2].textContent).to.eql('Curriculum PT-BR')
+    expect(vm.$el.querySelectorAll('.curriculum a')[1].textContent).to.contain('Certificates')
+    expect(vm.$el.querySelectorAll('.curriculum a')[1].href).to.eql('https://github.com/mauri870/certificados')
+
+    expect(vm.$el.querySelectorAll('.curriculum a')[2].textContent).to.contain('Curriculum PT-BR')
     expect(vm.$el.querySelectorAll('.curriculum a')[2].href).to.contains('static/docs/curricula/curriculum-pt.pdf')
   })
 })
