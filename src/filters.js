0faa268df284c3f0ee214f281c4365fbd8f55187
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-31 18:07:22 -0300

    Add filter for shuffle arrays (Durstenfeld shuffle method)

--- src/filters.js
+++ src/filters.js
@@ -3,3 +3,13 @@ import Vue from 'vue'
 Vue.filter('uppercase', (value) => {
   return value.toUpperCase()
 })
+
+Vue.filter('shuffleArray', (array) => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
+})
