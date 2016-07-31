ad7af11d3e8964423f64d3878358cb4bf7a412e4
Mauri de Souza Nunes <mauri870@gmail.com>
2016-07-31 18:07:38 -0300

    Move skills to vuex

--- src/store/modules/info.js
+++ src/store/modules/info.js
@@ -2,11 +2,13 @@ export default {
   state: {
     phrase: 'Web Designer, Developer, Gamer, History, science and astronomy lover',
     email: 'mauri870@gmail.com',
-    phone: '+55 54 8118-9792'
+    phone: '+55 54 8118-9792',
+    skills: ['LARAVEL', 'VUEJS', 'LUMEN', 'GIT', 'PHP', 'ʕ◔ϖ◔ʔ', 'WEBPACK', 'ELECTRON', 'MYSQL', 'SOLID', 'REST API\'S', 'HTML', 'CSS', 'BOOTSTRAP', 'BOWER', 'COMPOSER', 'NPM', 'LINUX', 'GULP', 'AWS']
   },
   mutations: {},
   getters: {
     infoPhrase: state => state.phrase,
-    infoEmail: state => state.email
+    infoEmail: state => state.email,
+    infoSkills: state => state.skills
   }
 }
