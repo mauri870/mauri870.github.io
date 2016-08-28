a22131028724d9897b3cbf665f0d0cf2bed5df44
Mauri de Souza Nunes <mauri870@gmail.com>
2016-08-28 17:04:55 -0300

    Add project and update projects techs

--- src/store/modules/projects.js
+++ src/store/modules/projects.js
@@ -5,8 +5,15 @@ export default {
         name: 'Powershell Reverse Http',
         link: 'https://github.com/mauri870/powershell-reverse-http',
         description: 'A Powershell exploit, windows native service with no virus signature that open a reverse http connection via meterpreter',
-        techs: ['Go'],
+        techs: ['Go', 'Exploit', 'Metasploit Framework'],
         image: require('../../assets/img/portfolio/exploit.png')
+      },
+      {
+        name: 'Cryptofile',
+        link: 'https://github.com/mauri870/cryptofile',
+        description: 'Encrypt or decrypt files using AES-256 or AES-128',
+        techs: ['Go', 'Security'],
+        image: require('../../assets/img/portfolio/security-padlock.png')
       },
       {
         name: 'Cloudradioo App',
@@ -26,28 +33,28 @@ export default {
         name: 'artesaos/laravel-docs',
         link: 'https://github.com/artesaos/laravel-docs',
         description: 'Translation of Laravel 5 docs to PT-BR',
-        techs: [],
+        techs: ['Laravel', 'Documentation'],
         image: require('../../assets/img/portfolio/artesaos-package.png')
       },
       {
         name: 'artesaos/laravel-installer',
         link: 'https://github.com/artesaos/laravel-installer',
         description: 'Install laravel framework with any version you want',
-        techs: [],
+        techs: ['Laravel'],
         image: require('../../assets/img/portfolio/artesaos-package.png')
       },
       {
         name: 'artesaos/defender',
         link: 'https://github.com/artesaos/defender',
         description: 'Roles & Permissions for Laravel 5',
-        techs: [],
+        techs: ['Laravel', 'Package'],
         image: require('../../assets/img/portfolio/artesaos-package.png')
       },
       {
         name: 'artesaos/laravel-linkedin',
         link: 'https://github.com/artesaos/laravel-linkedin',
         description: 'Linkedin API integration for Laravel and Lumen 5',
-        techs: [],
+        techs: ['Laravel', 'Package'],
         image: require('../../assets/img/portfolio/artesaos-package.png')
       }
     ]
