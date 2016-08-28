4245171c8c3f68961ff6527b0fa99a23561293d5
Mauri de Souza Nunes <mauri870@gmail.com>
2016-08-28 17:08:17 -0300

    Update tests

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -6,7 +6,7 @@ module.exports = {
       .assert.title('Mauri de Souza Nunes - Website')
       .assert.containsText('h3', 'My skills')
       .assert.elementPresent('.logo')
-      .assert.elementCount('.project', 7)
+      .assert.elementCount('.project', 8)
       .end()
   },
   'mauri870.github.io e2e test 404': browser => {
