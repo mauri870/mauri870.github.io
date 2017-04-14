fd5e4d2bfa7edb7ac8d06f41e580032b7b9a7dc1
Mauri de Souza Nunes <mauri870@gmail.com>
2017-04-14 01:34:13 -0300

    Fix tests

--- test/unit/specs/components/Header.spec.js
+++ test/unit/specs/components/Header.spec.js
@@ -21,8 +21,5 @@ describe('Header.vue', () => {
 
     expect(vm.$el.querySelectorAll('.curriculum a')[1].textContent).to.contain('Certificates')
     expect(vm.$el.querySelectorAll('.curriculum a')[1].href).to.eql('https://github.com/mauri870/certificados')
-
-    expect(vm.$el.querySelectorAll('.curriculum a')[2].textContent).to.contain('Curriculum PT-BR')
-    expect(vm.$el.querySelectorAll('.curriculum a')[2].href).to.contains('static/docs/curricula/curriculum-pt.pdf')
   })
 })
