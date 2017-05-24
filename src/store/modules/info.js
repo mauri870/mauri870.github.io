82487f5eeaef3800f13d38d065539d302389daaa
Mauri de Souza Nunes <mauri870@gmail.com>
2017-05-24 19:16:04 -0300

    Update info

--- src/store/modules/info.js
+++ src/store/modules/info.js
@@ -3,7 +3,7 @@ export default {
     phrase: 'Web Designer, Developer, Gamer, History, science and astronomy lover',
     email: 'mauri870@gmail.com',
     phone: '+55 54 8118-9792',
-    skills: ['LARAVEL', 'VUEJS', 'LUMEN', 'GO', 'REDIS', 'GIT', 'PHP', 'ʕ◔ϖ◔ʔ', 'WEBPACK', 'ELECTRON', 'MYSQL', 'SOLID', 'REST API\'S', 'HTML', 'CSS', 'BOOTSTRAP', 'BOWER', 'COMPOSER', 'NPM', 'LINUX', 'GULP', 'AWS', 'DOCKER', 'KERAS', 'TENSORFLOW', 'ZEPHIR']
+    skills: ['LARAVEL', 'VUEJS', 'LUMEN', 'GO', 'REDIS', 'GIT', 'PHP', 'ʕ◔ϖ◔ʔ', 'WEBPACK', 'ELECTRON', 'MYSQL', 'SOLID', 'REST API\'S', 'HTML', 'CSS', 'BOOTSTRAP', 'BOWER', 'COMPOSER', 'NPM', 'LINUX', 'GULP', 'AWS', 'DOCKER', 'KERAS', 'TENSORFLOW', 'ZEPHIR', 'GRPC', 'PROTOBUF']
   },
   mutations: {},
   getters: {
