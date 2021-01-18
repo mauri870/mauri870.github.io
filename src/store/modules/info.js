2d0c883461e3f7069562eb68e7b84d13edec06d2
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2021-01-18 08:17:42 -0300

    chore: update info skills

--- src/store/modules/info.js
+++ src/store/modules/info.js
@@ -4,7 +4,7 @@ export default {
     quote: 'Work hard. Dream big.',
     email: 'mauri870 [at] gmail [dot] com',
     phone: '+55 54 8118-9792',
-    skills: ['LARAVEL', 'VUEJS', 'LUMEN', 'GO', 'REDIS', 'GIT', 'JWT', 'LUA', 'NGINX', 'PHP', 'ʕ◔ϖ◔ʔ', 'WEBPACK', 'ELECTRON', 'MYSQL', 'SOLID', 'REST API\'S', 'HTML', 'CSS', 'BOOTSTRAP', 'BOWER', 'COMPOSER', 'NPM', 'LINUX', 'GULP', 'AWS', 'DOCKER', 'KERAS', 'TENSORFLOW', 'ZEPHIR', 'GRPC', 'PROTOBUF', 'BAZEL', 'GCLOUD', 'KUBERNETES', 'GITLAB', 'BASH'],
+    skills: ['LARAVEL', 'VUEJS', 'LUMEN', 'GO', 'REDIS', 'GIT', 'JWT', 'LUA', 'NGINX', 'PHP', 'ʕ◔ϖ◔ʔ', 'WEBPACK', 'ELECTRON', 'MYSQL', 'SOLID', 'REST API\'S', 'HTML', 'CSS', 'BOOTSTRAP', 'BOWER', 'COMPOSER', 'NPM', 'LINUX', 'GULP', 'AWS', 'DOCKER', 'KERAS', 'TENSORFLOW', 'ZEPHIR', 'GRPC', 'PROTOBUF', 'BAZEL', 'GCLOUD', 'KUBERNETES', 'GITLAB', 'BASH', 'PYTHON', 'RUST', 'ASTERISK', 'RTPPROXY', 'WEBSOCKETS', 'SOCKET.IO', 'GKE'],
   },
   mutations: {},
   getters: {
